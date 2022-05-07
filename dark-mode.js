dcc45bb51eed2adb05250491961011e6f17781cb
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-07 01:55:47 -0300

    responsividade do dark button

--- dark-mode.js
+++ dark-mode.js
@@ -13,8 +13,8 @@ function darkMode() {
     var strong2 = document.querySelector('.strOng2');
     var italic = document.querySelector('em');
     var italic2 = document.querySelector('.em');
-    var hability =document.querySelector('.minhas-habilidades');
-    var habilityOutras =document.querySelector('.outras-habilidades');
+    var hability = document.querySelector('.minhas-habilidades');
+    var habilityOutras = document.querySelector('.outras-habilidades');
 
     h1ToDark.classList.toggle("dark-mode");
     bodyToDark.classList.toggle("dark-mode-body");
@@ -29,9 +29,9 @@ function darkMode() {
     hability.classList.toggle("dark-mode-habilidades");
     habilityOutras.classList.toggle("dark-mode-outras");
 
-    if (counter%2 == 1){
-       icon = document.getElementById('darkModeImg').src="/assets/white-mode.png"; 
+    if (counter % 2 == 1) {
+        icon = document.getElementById('darkModeImg').src = "/assets/white-mode.png";
     } else {
-        icon = document.getElementById('darkModeImg').src="/assets/dark-mode.png";
+        icon = document.getElementById('darkModeImg').src = "/assets/dark-mode.png";
     }
 }
